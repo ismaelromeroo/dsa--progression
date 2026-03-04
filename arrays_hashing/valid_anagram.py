9849b83d2998e094c20ebd5b862161d9852771a1
ismaelromeroo <ism.romerr@gmail.com>
2026-03-03 23:35:10 -0500

    word on day 2 which included 2 sum and group anagrams

--- arrays_hashing/valid_anagram.py
+++ arrays_hashing/valid_anagram.py
@@ -1,9 +1,16 @@
 """
-Problem: Valid Anagram
-Pattern: HashMap / Frequency Counting
-Time Complexity: O(n)
-Space Complexity: O(n)
+Pattern: sorting
+Time Complexity: o(n log n)
+Space Complexity:o(n)
 
-Key Idea:
-Count characters and compare frequencies.
+Key Idea: two sting are anagrams if same length and same unique characters + frequencies
+function: we use the sorted function to make the strings sorted list and comapre to establish if they are anagrams or not
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+
+can also use counter(s) == counter(t)
+which is o(n) and o(n) since sorting adds an o(n) causing the n log n time complexity
 """
